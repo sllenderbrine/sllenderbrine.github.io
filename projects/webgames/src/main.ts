79f70ff7ae1ae4954689a2d0687992579275529e
sllenderbrine <sllenderbrine@gmail.com>
2026-03-05 06:50:27 -0600

    room link example

--- projects/webgames/src/main.ts
+++ projects/webgames/src/main.ts
@@ -1,4 +1,6 @@
 import LoginPrompt from "./pages/login.js";
 import { conn, onSubscriptionApplied } from "./spacetime.js";
 
+// https://sllenderbrine.github.io/projects/webgames/?room=kbNbE
+
 let prompt = new LoginPrompt();
