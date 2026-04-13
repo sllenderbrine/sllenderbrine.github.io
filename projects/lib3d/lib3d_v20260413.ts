abe3cdd9fd54136dc77c85d4476b50e1a29f68ab
sllenderbrine <sllenderbrine@gmail.com>
2026-04-13 14:28:46 -0500

    add render loop class

--- projects/lib3d/lib3d_v20260413.ts
+++ projects/lib3d/lib3d_v20260413.ts
@@ -2084,3 +2084,37 @@ export class Color {
         return [this.r, this.g, this.b, this.a];
     }
 }
+
+export class RenderLoop {
+    runIndex = 0;
+    isRunning = false;
+    constructor(public callback: (dt: number) => void) {
+
+    }
+    stop() {
+        if(!this.isRunning)
+            return this;
+        this.isRunning = false;
+        this.runIndex++;
+        return this;
+    }
+    start() {
+        if(this.isRunning)
+            return this;
+        this.isRunning = true;
+        let ri = this.runIndex;
+        let frameTime = performance.now()/1000;
+        const render = () => {
+            if(this.runIndex != ri) {
+                return;
+            }
+            let now = performance.now()/1000;
+            let dt = now - frameTime;
+            frameTime = now;
+            this.callback(dt);
+            requestAnimationFrame(render);
+        }
+        render();
+        return this;
+    }
+}
