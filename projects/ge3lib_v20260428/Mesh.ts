5ae608ab3352a75aaa365b268de36146588eb0a4
sllenderbrine <sllenderbrine@gmail.com>
2026-04-28 19:51:24 -0500

    test scene

--- projects/ge3lib_v20260428/Mesh.ts
+++ projects/ge3lib_v20260428/Mesh.ts
@@ -25,14 +25,14 @@ export class Mesh {
             normal[1] = this.normals[positionIndex+1]!;
             normal[2] = this.normals[positionIndex+2]!;
             callback(position, texcoord, normal);
-            this.positions[i] = position[0];
-            this.positions[i+1] = position[1];
-            this.positions[i+2] = position[2];
-            this.texcoords[i] = texcoord[0];
-            this.texcoords[i+1] = texcoord[1];
-            this.normals[i] = normal[0];
-            this.normals[i+1] = normal[1];
-            this.normals[i+2] = normal[2];
+            this.positions[positionIndex] = position[0];
+            this.positions[positionIndex+1] = position[1];
+            this.positions[positionIndex+2] = position[2];
+            this.texcoords[texcoordIndex] = texcoord[0];
+            this.texcoords[texcoordIndex+1] = texcoord[1];
+            this.normals[positionIndex] = normal[0];
+            this.normals[positionIndex+1] = normal[1];
+            this.normals[positionIndex+2] = normal[2];
         }
         return this;
     }
--- projects/z_testscene/main.js
+++ projects/z_testscene/main.js
@@ -1,4 +1,4 @@
-import { Camera3D, Keypresses, Mesh, RenderLoop, Vec3, WGL2Shader } from "../gelib3_v20260428/index.js";
+import { Camera3D, Keypresses, Mesh, RenderLoop, Vec3, WGL2Shader } from "../ge3lib_v20260428/index.js";
 
 const canvas = document.createElement("canvas");
 document.body.appendChild(canvas);
@@ -8,55 +8,82 @@ canvas.style = `
     image-rendering: pixelated;
     object-fit: contain;
 `;
-canvas.width = 640;
-canvas.height = 360;
+canvas.width = 1280;
+canvas.height = 720;
 const gl = canvas.getContext("webgl2");
 
 const shader = new WGL2Shader(
     gl,
     `#version 300 es
         in vec3 a_position;
+        in vec3 a_normal;
         uniform mat4 u_perspective;
         uniform mat4 u_view;
+        uniform vec3 u_sun_direction;
+        out float v_lighting;
         void main() {
             gl_Position = u_perspective * u_view * vec4(a_position, 1);
+            v_lighting = dot(a_normal, u_sun_direction) * 0.25 + 0.75;
         }
     `, `#version 300 es
         precision highp float;
+        in float v_lighting;
         out vec4 outColor;
         void main() {
-            outColor = vec4(1, 0, 0, 1);
+            outColor = vec4(vec3(1, 0, 0) * v_lighting, 1);
         }
     `
 );
 const uPerspective = shader.createUniform("u_perspective", "mat4");
 const uView = shader.createUniform("u_view", "mat4");
+const uSunDirection = shader.createUniform("u_sun_direction", "vec3");
+let sunDir = new Vec3(1,3,2);
+sunDir.normPut(sunDir);
+uSunDirection.setValues(sunDir.toArray());
 const aPosition = shader.addAttribute("a_position", "vec3");
+const aNormal = shader.addAttribute("a_normal", "vec3");
 
 const camera = new Camera3D();
+camera.position = new Vec3(4, 4, 4);
+camera.lookAt(new Vec3(0, 0, 0));
+camera.invalidateView();
 camera.aspect = canvas.width / canvas.height;
 camera.invalidateProjection();
 
 const planeFront = new Mesh();
 planeFront.positions.push(-1,-1,1, 1,-1,1, 1,1,1, -1,-1,1, 1,1,1, -1,1,1);
 planeFront.texcoords.push(0,0, 1,0, 1,1, 0,0, 0,1, 1,1);
-planeFront.normals.push(0,0,1, 0,0,1, 0,0,1, 0,0,1, 0,0,1, 0,0,1, 0,0,1);
+planeFront.normals.push(0,0,1, 0,0,1, 0,0,1, 0,0,1, 0,0,1, 0,0,1);
 const tmp0 = Vec3.zero();
+const planeLeft = planeFront.clone().transformSelf((position, texcoord, normal) => {
+    tmp0.fromArray(position).rotateYPut(Math.PI/2, tmp0).toArrayPut(position);
+    tmp0.fromArray(normal).rotateYPut(Math.PI/2, tmp0).toArrayPut(normal);
+});
 const planeBack = planeFront.clone().transformSelf((position, texcoord, normal) => {
-    tmp0.fromArray(position);
-    tmp0.rotateYPut(Math.PI, tmp0);
-    tmp0.toArrayPut(position);
-    tmp0.fromArray(normal);
-    tmp0.rotateYPut(Math.PI, tmp0);
-    tmp0.toArrayPut(normal);
+    tmp0.fromArray(position).rotateYPut(Math.PI, tmp0).toArrayPut(position);
+    tmp0.fromArray(normal).rotateYPut(Math.PI, tmp0).toArrayPut(normal);
+});
+const planeRight = planeFront.clone().transformSelf((position, texcoord, normal) => {
+    tmp0.fromArray(position).rotateYPut(3*Math.PI/2, tmp0).toArrayPut(position);
+    tmp0.fromArray(normal).rotateYPut(3*Math.PI/2, tmp0).toArrayPut(normal);
+});
+const planeBottom = planeFront.clone().transformSelf((position, texcoord, normal) => {
+    tmp0.fromArray(position).rotateXPut(Math.PI/2, tmp0).toArrayPut(position);
+    tmp0.fromArray(normal).rotateXPut(Math.PI/2, tmp0).toArrayPut(normal);
+});
+const planeTop = planeFront.clone().transformSelf((position, texcoord, normal) => {
+    tmp0.fromArray(position).rotateXPut(3*Math.PI/2, tmp0).toArrayPut(position);
+    tmp0.fromArray(normal).rotateXPut(3*Math.PI/2, tmp0).toArrayPut(normal);
 });
 const cube = new Mesh().concatSelf(
-    planeFront, planeBack
+    planeFront, planeLeft, planeBack, planeRight, planeBottom, planeTop
 );
 const cubePositions = new Float32Array(cube.positions);
+const cubeNormals = new Float32Array(cube.normals);
 
 const shaderObj = shader.createObject();
 shaderObj.setData(aPosition.name, cubePositions);
+shaderObj.setData(aNormal.name, cubeNormals);
 
 gl.enable(gl.DEPTH_TEST);
 gl.enable(gl.CULL_FACE);
